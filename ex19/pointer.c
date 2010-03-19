e0e258af7682fb4cd77d2a16e20b3ab6fa5e9b97
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-03-19 22:32:10 +0200

    Includes, cleanup

--- ex19/pointer.c
+++ ex19/pointer.c
@@ -3,11 +3,23 @@
  * OHJ-2050 Exercise 19
  * Ilkka Laukkanen
  */
+#include <stdlib.h>
+
+#define SIZE 1000
 
 int main(int argc, char* argv[])
 {
+    int array[SIZE][SIZE];
+    int count = SIZE * SIZE;
+
     (void)argc;
     (void)argv;
+
+    srand(1);
+
+    while(count > 0) {
+    }
+
     return 0;
 }
 
