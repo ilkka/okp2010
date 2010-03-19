b5ad18334ab807842fbffd217c6c318dde45fbc0
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-03-19 22:27:58 +0200

    Perform array accesses with random indices

--- ex19/subscript.c
+++ ex19/subscript.c
@@ -4,10 +4,27 @@
  * Ilkka Laukkanen
  */
 
+#include <stdlib.h>
+
+#define SIZE 1000
+
 int main(int argc, char* argv[])
 {
+    int array[SIZE][SIZE];
+    int count = SIZE * SIZE;
+
     (void)argc;
     (void)argv;
+
+    srand(1);
+
+    while (count > 0) {
+        int x = (int) (((double)SIZE) * (rand() / (RAND_MAX + 1.0)));
+        int y = (int) (((double)SIZE) * (rand() / (RAND_MAX + 1.0)));
+        array[x][y] = rand();
+        --count;
+    }
+
     return 0;
 }
 
